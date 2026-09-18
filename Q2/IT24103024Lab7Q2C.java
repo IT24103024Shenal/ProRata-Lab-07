bb3bed5fc618b77ac382e050800c1b4ba27f0cbd
Ramidu Shenal <it24103024@my.sliit.lk>
2026-09-18 12:16:10 +0530

    Add files via upload

---
+++ Q2/IT24103024Lab7Q2C.java
@@ -0,0 +1,10 @@
+public class IT24103024Lab7Q2C {
+    public static void main(String[] args) {
+        for (int num = 5; num >= 1; num--) {
+            for (int col = 1; col <= num; col++) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
